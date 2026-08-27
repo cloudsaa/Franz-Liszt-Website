fd5a9a62e5ddda5ec323dca98456eec76dbed88d
cloudsaa <liviasala700@gmail.com>
2026-08-27 12:05:59 +0200

    video section

--- src/main.js
+++ src/main.js
@@ -1,1 +1,36 @@
 import './style.css'
+
+const videos = document.querySelectorAll(".video");
+
+const previousButton = document.querySelector("#previous");
+const nextButton = document.querySelector("#next");
+
+let currentVideo = 0;
+
+function showVideo(index) {
+    videos.forEach(video => {
+        video.classList.add("hidden");
+    });
+
+    videos[index].classList.remove("hidden");
+}
+
+nextButton.addEventListener("click", () => {
+    currentVideo++;
+
+    if (currentVideo >= videos.length) {
+        currentVideo = 0;
+    }
+
+    showVideo(currentVideo);
+});
+
+previousButton.addEventListener("click", () => {
+    currentVideo--;
+
+    if (currentVideo < 0) {
+        currentVideo = videos.length - 1;
+    }
+
+    showVideo(currentVideo);
+});
